a474ae798eed61dc9e4503f51c78f62e6aaafb47
alex <alexpmathai@gmail.com>
2025-04-21 21:38:16 -0400

    create search for teams and players

--- home/views.py
+++ home/views.py
@@ -2,3 +2,23 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'home/index.html')
+
+def player_search_view(request):
+    query = request.GET.get('q', '')
+    players = Player.objects.filter(name__icontains=query) if query else []
+
+    context = {
+        'query': query,
+        'players': players,
+    }
+    return render(request, 'home/player_search.html', context)
+
+def team_search_view(request):
+    query = request.GET.get('q', '')
+    teams = Team.objects.filter(name__icontains=query) if query else []
+
+    context = {
+        'query': query,
+        'teams': teams,
+    }
+    return render(request, 'home/team_search.html', context)
