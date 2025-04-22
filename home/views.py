772010e51880b49044e11681bf660ffb22abcc1e
alex <alexpmathai@gmail.com>
2025-04-22 16:17:43 -0400

    basic search for players. players come up but display generic plus you cant see any information.

--- home/views.py
+++ home/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+from nba_api.stats.static import players
+
 
 def homepage(request):
     return render(request, 'home/index.html')
 
 def player_search_view(request):
     query = request.GET.get('q', '')
-    players = Player.objects.filter(name__icontains=query) if query else []
+    matched_players = []
+
+    if query:
+        all_players = players.get_players()
+        query_lower = query.lower()
+        matched_players = [
+            p for p in all_players if query_lower in p['full_name'].lower()
+        ]
 
     context = {
         'query': query,
-        'players': players,
+        'players': matched_players
     }
     return render(request, 'home/player_search.html', context)
 
