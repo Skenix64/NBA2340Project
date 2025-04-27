b05d8b7c7ca7a0640aa8607ab6d53bf586f9d62f
clxirelee <clee931@gatech.edu>
2025-04-27 14:39:49 -0400

    Finalize arena map features, ready to merge

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('players/', views.player_search_view, name='player_search'),
+    path('teams/', views.team_search_view, name='team_search'),
 ]
